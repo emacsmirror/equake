198d39ceada2e1e265c64f95f60e251adda8f65a
Benjamin Slade <slade@jnanam.net>
2019-01-02 17:24:30 -0700

    fix remembering current tab

--- equake.el
+++ equake.el
@@ -501,7 +501,7 @@ external function call to 'equake-invoke'.")
             (progn (equake-store-window-history) ; store window history.
                    ;; (set-frame-parameter equake-current-frame 'fullscreen 'nil) ; make sure to unfullscreen.
                    ;; ;; Emacs Frame Visibility Rule #2: Double-Tap - "one more makes 100% sure":
-                   ;; (make-frame-invisible equake-current-frame)
+                  (make-frame-invisible equake-current-frame)
                    (make-frame-invisible equake-current-frame))
           (if (cl-search monitorid (frame-parameter equake-current-frame 'name))
               ;; SOMETHING HERE TO MAKE SURE WE'RE RAISING THE RIGHT ONE
@@ -700,7 +700,7 @@ external function call to 'equake-invoke'.")
               (setq mode-line-format (list (equake-mode-line (concat monitorid ": ") (equake-find-monitor-list monitorid equake-tab-list))))
             (setq mode-line-format (list (equake-mode-line "" (equake-find-monitor-list monitorid equake-tab-list)))))
           (force-mode-line-update)
-          (equake-set-current-etab)
+          ;; (equake-set-current-etab)
           ;;  (set-frame-parameter (selected-frame) 'menu-bar-lines 0) ; no menu-bars
           (modify-frame-parameters (selected-frame) '((vertical-scroll-bars . nil) (horizontal-scroll-bars . nil)))) ; no scrollbars
       (setq inhibit-message 'nil)

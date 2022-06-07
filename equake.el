860c6a927118158cb5afd9be6b4b8ac139d9c0f8
Benjamin Slade <slade@lambda-y.net>
2022-06-07 14:12:12 -0600

    add new function to transform frame into Equake frame
    - this is a workaround for GNOME Shell running under Wayland
      - the workaround works by:
        (a) destroying frames rather than hiding them
        (b) having the user run a shell script that creates a new emacsclient frame,
             focusses it, and then calls a function to transform that emacsclient
             frame into an Equake frame
    
    - the "magic" command is:
    emacsclient -c -e "(progn (select-frame-set-input-focus (selected-frame))
                                  (equake--transform-existing-frame-into-equake-frame)
                                  (goto-char (1- (point-max))))"
      - The first makes sure the frame ("window" in WM/DE parlance ) is on top
      - The second runs the transformation function
      - The third is aworkaround for the point not being at the last command
    
    - addresses issue https://gitlab.com/emacsomancer/equake/-/issues/27
    
    - now =equake--set-up-new-frame= and
    =equake--transform-existing-frame-into-equake-frame= share a lot of overlapping
    code, should be refactored

--- equake.el
+++ equake.el
@@ -795,6 +795,35 @@ frame is destroyed."
     (raise-frame)
     (set-frame-parameter frame 'equakep t))) ; mark a finished Equake frame
 
+(defun equake--transform-existing-frame-into-equake-frame ()
+  "For GNOME Shell under Wayland, used with an emacslient call."
+  ;; N.B. the resulting frame should be marked as a finished Equake
+  ;; frame only when it's fully configured. That means,
+  ;; `(set-frame-parameter frame 'equakep t)' only at the end of
+  ;; initialization. Otherwise, things will break.
+  (let* ((frame (selected-frame))
+         (monitor (equake--get-monitor frame))
+         (workarea (equake--get-monitor-attribute 'workarea)))
+    (setf (alist-get monitor equake--frame) frame)
+    (modify-frame-parameters frame (equake--make-frame-parameters monitor workarea))
+    ;; (select-frame frame)
+    (unless (alist-get monitor equake--tab-list)
+      (equake-new-tab))
+    (->> (alist-get monitor equake--win-history)
+         (equake--filter-history)
+         (set-window-prev-buffers nil))
+    (when-let ((last-tab (alist-get monitor equake--last-tab)))
+      (switch-to-buffer last-tab))
+    (when-let ((last-buffer (alist-get monitor equake--last-buffer)))
+      (switch-to-buffer last-buffer))
+    (unless (equake--tab-p) ; make sure to restore to an Equake buffer
+      (bury-buffer))
+    (buffer-face-set 'equake-buffer-face)
+    (when equake-hide-from-taskbar-choice
+      (equake--hide-from-taskbar))
+    (raise-frame)
+    (set-frame-parameter frame 'equakep t))) ; mark a finished Equake frame
+
 (defun equake--make-new-frame ()
   "Make a new Equake frame on a current monitor on a current display.
 
